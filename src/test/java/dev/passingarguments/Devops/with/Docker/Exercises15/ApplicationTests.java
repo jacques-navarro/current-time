f361dc2dd77fdf5e19b99134ea520a711b4c0a1b
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-04-27 00:00:54 +0200

    Initial commit

---
+++ src/test/java/dev/passingarguments/Devops/with/Docker/Exercises15/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.passingarguments.Devops.with.Docker.Exercises15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
