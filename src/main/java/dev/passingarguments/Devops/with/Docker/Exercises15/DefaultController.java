014c375cbb502387f3144844eb843bdb08bba455
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-04-27 00:37:04 +0200

    Add display-time.html

--- src/main/java/dev/passingarguments/Devops/with/Docker/Exercises15/DefaultController.java
+++ src/main/java/dev/passingarguments/Devops/with/Docker/Exercises15/DefaultController.java
@@ -1,7 +1,10 @@
 package dev.passingarguments.Devops.with.Docker.Exercises15;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class DefaultController {
@@ -10,4 +13,14 @@ public class DefaultController {
     public String getHome() {
         return "home";
     }
+
+    @GetMapping("/getTime")
+    public String displayTime(Model model) {
+        var currentTime = LocalDateTime.now();
+        System.out.println(currentTime);
+        model.addAttribute("currentTime", currentTime);
+        return "display-time";
+    }
+
+
 }
